66404cf842e8f2c2722f4da13515c9b78c0f2612
김준현 <smileehd03@dgsw.hs.kr>
2026-03-31 13:50:44 +0900

    refactor 웹소켓 세션 -> 웹소켓 jwt

--- src/main/java/spring/springserver/global/config/WebSocketConfig.java
+++ src/main/java/spring/springserver/global/config/WebSocketConfig.java
@@ -2,19 +2,19 @@ package spring.springserver.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import spring.springserver.global.handler.CustomHandshakeHandler;
-import spring.springserver.global.jwt.JwtProvider;
+import spring.springserver.global.config.WebSocketChannelInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final JwtProvider jwtProvider;
+    private final WebSocketChannelInterceptor webSocketChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -27,8 +27,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
                 .setAllowedOrigins("*")
-                .addInterceptors(new HttpSessionIdHandshakeInterceptor(jwtProvider))
-                .setHandshakeHandler(new CustomHandshakeHandler())
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketChannelInterceptor);
     }
 }
