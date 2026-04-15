8141b4d9c8b68f5b20343a1e0566806334fb4573
김승우 <me.secur1ty.dev@gmail.com>
2026-04-15 20:12:08 +0900

    fix: 불 필요한 코드 제거

--- src/main/java/spring/springserver/domain/member/entity/Member.java
+++ src/main/java/spring/springserver/domain/member/entity/Member.java
@@ -3,11 +3,9 @@ package spring.springserver.domain.member.entity;
 import com.l98293.phone.Phone;
 import com.l98293.phone.Region;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Entity
