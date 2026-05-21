b7714fda62f6dce404567b7b9c056499104d8526
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2026-05-21 20:39:11 +0900

    fix: convention

--- src/main/java/spring/springserver/domain/post/exception/PostStatusCode.kt
+++ src/main/java/spring/springserver/domain/post/exception/PostStatusCode.kt
@@ -6,6 +6,7 @@ import spring.springserver.global.exception.status_code.StatusCode
 enum class PostStatusCode(private val code: String,
                           private val message: String? = null,
                           private val httpStatus: HttpStatus? = null) : StatusCode {
+    
     FORBIDDEN_POST_ACCESS("FORBIDDEN_POST_ACCESS", "본인이 작성한 게시글만 수정 또는 삭제할 수 있습니다.", HttpStatus.FORBIDDEN),
     INVALID_POST("INVALID_POST", "존재하지 않는 게시글입니다.", HttpStatus.BAD_REQUEST);
 
