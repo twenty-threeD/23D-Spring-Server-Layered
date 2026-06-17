602043759780307bcbadaafd7d215c40664750f2
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2026-06-17 17:26:59 +0900

    fix: convention

--- src/main/java/spring/springserver/domain/post/data/request/UpdatePostRequest.kt
+++ src/main/java/spring/springserver/domain/post/data/request/UpdatePostRequest.kt
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
 data class UpdatePostRequest(
+    @field:NotBlank
     val id: Long,
 
     @field:NotBlank
--- src/main/java/spring/springserver/domain/post/exception/PostStatusCode.kt
+++ src/main/java/spring/springserver/domain/post/exception/PostStatusCode.kt
@@ -3,9 +3,10 @@ package spring.springserver.domain.post.exception
 import org.springframework.http.HttpStatus
 import spring.springserver.global.exception.status_code.StatusCode
 
-enum class PostStatusCode(private val code: String,
-                          private val message: String,
-                          private val httpStatus: HttpStatus): StatusCode {
+enum class PostStatusCode(
+    private val code: String,
+    private val message: String,
+    private val httpStatus: HttpStatus): StatusCode {
 
     FORBIDDEN_POST_ACCESS("FORBIDDEN_POST_ACCESS", "본인이 작성한 게시글만 수정 또는 삭제할 수 있습니다.", HttpStatus.FORBIDDEN),
     INVALID_POST("INVALID_POST", "존재하지 않는 게시글입니다.", HttpStatus.NOT_FOUND);
