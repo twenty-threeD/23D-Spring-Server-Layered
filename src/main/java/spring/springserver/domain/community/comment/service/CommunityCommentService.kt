a23ea463fc877d881bf3e5056ed04db5a1119d7e
김준현 <smileehd03@dgsw.hs.kr>
2026-06-17 10:38:11 +0900

    fix: 댓글 최신순으로 정렬 및 일부 불필요한 코드 수정

--- src/main/java/spring/springserver/domain/community/comment/service/CommunityCommentService.kt
+++ src/main/java/spring/springserver/domain/community/comment/service/CommunityCommentService.kt
@@ -3,14 +3,13 @@ package spring.springserver.domain.community.comment.service
 import spring.springserver.domain.community.comment.data.request.CreateCommentRequest
 import spring.springserver.domain.community.comment.data.request.UpdateCommentRequest
 import spring.springserver.domain.community.comment.data.response.CommunityCommentResponse
-import spring.springserver.domain.community.comment.entity.CommunityComment
 import spring.springserver.domain.community.common.data.response.DeleteResponse
 
 interface CommunityCommentService {
 
     fun createComment(createCommentRequest: CreateCommentRequest): CommunityCommentResponse
 
-    fun getComments(postId: Long): List<CommunityComment>
+    fun getComments(postId: Long): List<CommunityCommentResponse>
 
     fun updateComment(updateCommentRequest: UpdateCommentRequest): CommunityCommentResponse
 
