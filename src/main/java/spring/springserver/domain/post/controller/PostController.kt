8ea07ee3eed6cdecad2e9ea57ffa44f81e01e57e
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2026-06-18 17:03:41 +0900

    fix: Controller

--- src/main/java/spring/springserver/domain/post/controller/PostController.kt
+++ src/main/java/spring/springserver/domain/post/controller/PostController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
@@ -26,7 +27,7 @@ class PostController(
 
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun createPost(
-        @Valid @RequestPart("request") createPostRequest: CreatePostRequest,
+        @Valid @RequestBody createPostRequest: CreatePostRequest,
         @RequestPart("multipartFile", required = false) multipartFile: MultipartFile?
     ): BaseResponse<PostResponse> {
 
@@ -43,7 +44,7 @@ class PostController(
 
     @PatchMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun updatePost(
-        @Valid @RequestPart("request") updatePostRequest: UpdatePostRequest,
+        @Valid @RequestBody updatePostRequest: UpdatePostRequest,
         @RequestPart("multipartFile", required = false) multipartFile: MultipartFile?
     ): BaseResponse<PostResponse> {
 
