af0283a2a95841e6f650b7e90a3e924cbd6b420a
김경윤 <l982930009@gmail.com>
2026-07-06 16:05:31 +0900

    fix: require email when signin

--- src/main/java/spring/springserver/domain/auth/data/request/SignInRequest.kt
+++ src/main/java/spring/springserver/domain/auth/data/request/SignInRequest.kt
@@ -1,10 +1,12 @@
 package spring.springserver.domain.auth.data.request
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 
 data class SignInRequest(
+    @field:Email
     @field:NotBlank
-    val username: String,
+    val email: String,
 
     @field:NotBlank
     val password: String
