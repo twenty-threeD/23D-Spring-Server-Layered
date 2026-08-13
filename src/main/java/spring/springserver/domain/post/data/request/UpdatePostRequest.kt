2744e8493d4f2f7f87f82bd4f5f66d0cd8f66dba
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2026-08-13 17:14:46 +0900

    fix: Id field Notblank to NotNull

--- src/main/java/spring/springserver/domain/post/data/request/UpdatePostRequest.kt
+++ src/main/java/spring/springserver/domain/post/data/request/UpdatePostRequest.kt
@@ -1,11 +1,12 @@
 package spring.springserver.domain.post.data.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.Size
 
 data class UpdatePostRequest(
-    @field:NotBlank
+    @field:NotNull
     val id: Long,
 
     @field:NotBlank
