50acefba208b5c3a33d6f58eaee0911c38a5a882
김경윤 <143334643+L98293@users.noreply.github.com>
2026-09-23 13:33:54 +0900

    feat: add image cache

--- src/main/java/spring/springserver/global/config/WebConfig.kt
+++ src/main/java/spring/springserver/global/config/WebConfig.kt
@@ -1,10 +1,12 @@
 package spring.springserver.global.config
 
+import org.springframework.http.CacheControl
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.nio.file.Path
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class WebConfig(
@@ -18,5 +20,10 @@ class WebConfig(
 
         resourceHandlerRegistry.addResourceHandler("/files/**")
             .addResourceLocations("file:${Path.of(fileDir).toAbsolutePath().normalize()}/")
+            .setCacheControl(
+                CacheControl.maxAge(7, TimeUnit.DAYS)
+                    .cachePublic()
+                    .immutable()
+            )
     }
 }
